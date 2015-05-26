430c41820eb4e5a86c3f8429279e47cb51bd4ac4
Esteban Gaete <zoraskiller@gmail.com>
2015-05-26 18:01:28 -0300

    Initial Lab03 commit

---
+++ hotpotato.cpp
@@ -0,0 +1,5 @@
+/***********************************************
+ * LAB3: MPI 1
+ * Desarrollado por: Esteban Gaete Flores
+ * USACH - Ramo: Taller de Programación Paralela
+ ***********************************************/
